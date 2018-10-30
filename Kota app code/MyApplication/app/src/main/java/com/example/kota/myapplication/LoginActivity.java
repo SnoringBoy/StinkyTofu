0d012cf12c46c1d0572640f1424998f1204c85ae
KotaIn <44621954+KotaIn@users.noreply.github.com>
2018-10-31 06:31:08 +0900

    app code
    Kota's code

---
+++ Kota app code/MyApplication/app/src/main/java/com/example/kota/myapplication/LoginActivity.java
@@ -0,0 +1,56 @@
+package com.example.kota.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+//                    mTextMessage.setText("Home");
+                    finish();
+                    return true;
+                case R.id.navigation_items:
+                    mTextMessage.setText("Item");
+                    return true;
+                case R.id.navigation_purchase:
+                    mTextMessage.setText("purchase");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+//    public void onClick(View view){
+//        Intent intent;
+//        switch (view.getId()){
+//            case R.id.navigation_home:
+//                finish();
+//                break;
+//        }
+//    }
+
+}
---
+++ Kota app code/MyApplication/app/src/main/java/com/example/kota/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.kota.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_items:
+                    return true;
+                case R.id.navigation_purchase:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void onClick(View view){
+        Intent intent;
+        switch (view.getId()){
+            case R.id.button:
+
+                intent = new Intent(this, LoginActivity.class);
+
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                intent = new Intent(this, guestActivity.class);
+
+                startActivity(intent);
+                break;
+        }
+    }
+}
